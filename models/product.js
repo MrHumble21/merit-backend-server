dd376d504ca227a2e4781d5ae4f0584ddcdf8931
MunisaR <munisrahmatullayeva04@gmail.com>
2023-02-21 12:33:19 +0500

    feat: changed name

--- models/product.js
+++ models/product.js
@@ -1,32 +1,30 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const CategorySchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    code: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    amount: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    isPopular: {
-        type: Boolean,
-        default: false,
-        required: true
-    }
-
-
+const Productchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  isPopular: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
 });
 
-exports.CategorySchema = CategorySchema;
+exports.Productchema = Productchema;
