8521deae2a9a15b800ac39c5f08e1a4574e6003b
MunisaR <munisrahmatullayeva04@gmail.com>
2023-02-21 12:27:08 +0500

    feat: added database

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
 const connectionString =
-  "mongodb+srv://Abdulboriy:itransition@cluster0.mpywc.mongodb.net/?retryWrites=true&w=majority&wtimeoutMS=5000/collections";
+  "mongodb+srv://armeum:armeum@armeumdev.owmsjm0.mongodb.net/?retryWrites=true&w=majority/merit";
 
 exports.connectionString = connectionString;
